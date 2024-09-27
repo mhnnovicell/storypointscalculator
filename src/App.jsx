8ddb8e937eecb4ca09527fde15658bf8b9c8e93c
NRGI\mhni <mhni@nrgi.dk>
2024-09-27 11:48:00 +0200

    More changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Suspense } from "react";
+
+import CalculateStoryPoints from "./components/CalculateStoryPoints";
+import Loading from "./components/Loading";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Suspense fallback={<Loading />}>
+        <CalculateStoryPoints />
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
--- src/components/CalculateStoryPoints.tsx
+++ src/components/CalculateStoryPoints.tsx
@@ -1,60 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CalculateStoryPoints() {
+  const [hours, setHours] = useState("");
+  const [storyPoints, setStoryPoints] = useState("2.2");
+
+  const calculateDoubledStoryPoints = () => {
+    const hoursAsNumber = Number(hours);
+    const storyPointsAsNumber = parseFloat(storyPoints.replace(",", "."));
+    return (hoursAsNumber * storyPointsAsNumber).toFixed(2);
+  };
+
   return (
-    <form className="max-w-sm mx-auto">
+    <form className="flex flex-col items-center justify-center max-w-sm p-10 mx-auto">
+      <p>
+        <b>Formular:</b>
+      </p>
+      <p>
+        <b>timer * storypoints</b>
+      </p>
       <div className="mb-5">
         <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          htmlFor="hours"
+          className="block my-2 text-sm font-medium text-gray-900"
         >
-          Your email
+          Timer
         </label>
         <input
-          type="email"
-          id="email"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="name@flowbite.com"
+          value={hours}
+          onChange={(e) => setHours(e.target.value)}
+          type="number"
+          id="hours"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+          placeholder="8 timer = 1 dag"
           required
         />
       </div>
       <div className="mb-5">
         <label
-          htmlFor="password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          htmlFor="storypoints"
+          className="block mb-2 text-sm font-medium text-gray-900 "
         >
-          Your password
+          Storypoints
         </label>
         <input
-          type="password"
-          id="password"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          type="number"
+          id="storypoints"
+          placeholder="2.2"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
           required
+          value={storyPoints}
+          onChange={(e) => setStoryPoints(e.target.value)}
         />
       </div>
-      <div className="flex items-start mb-5">
-        <div className="flex items-center h-5">
-          <input
-            id="remember"
-            type="checkbox"
-            value=""
-            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-            required
-          />
-        </div>
-        <label
-          htmlFor="remember"
-          className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-        >
-          Remember me
-        </label>
-      </div>
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Submit
-      </button>
+      {Number(hours) > 0 && (
+        <>
+          <b>
+            <p>Resultat:</p>
+            <p> {calculateDoubledStoryPoints()}</p>
+          </b>
+        </>
+      )}
     </form>
   );
 }
